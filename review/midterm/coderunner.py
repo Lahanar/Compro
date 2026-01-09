5ff0925afbb5ea14757542ae529fd76781a3547e
Lahanar <144640552+Lahanar@users.noreply.github.com>
2026-01-09 16:48:44 +0700

    Update coderunner.py - update hint of q.2

--- review/midterm/coderunner.py
+++ review/midterm/coderunner.py
@@ -11,7 +11,7 @@ def run_grader():
             "expected": lambda d: f"{math.pi * d['radius']**2:.2f}"
         },
         "2. List Ops": {
-            "hint": "print to see the original value in variable 'score'. \nThen, insert value 5 to the beginning of 'score'.\n Next, add value 30 at the end.\nStore in 'result'.", 
+            "hint": "print to see the original value in variable 'score'. \nThen, insert value 5 at the beginning.\n Next, add value 30 at the end.\nStore in 'result'.", 
             "tests": [{"score": [10, 20]}], "target": "result", 
             "expected": lambda d: [5, 10, 20, 30]
         },
