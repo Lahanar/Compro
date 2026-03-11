9a3f07d53b7cf0c277236fe6ecffc3ff2ca68ce8
Lahanar <144640552+Lahanar@users.noreply.github.com>
2026-03-11 08:26:13 +0700

    Update questions and choices in question.js

--- python-quiz/question.js
+++ python-quiz/question.js
@@ -1,32 +1,223 @@
 const questions = [
 
 {
-question: "What keyword is used to define a function in Python?",
-choices: ["function","def","func","define"],
+question: "What is a main problem when developing large programs without functions?",
+choices: [
+"Program runs faster",
+"Code becomes hard to read and maintain",
+"Program uses less memory",
+"No errors occur"
+],
+answer: "Code becomes hard to read and maintain"
+},
+
+{
+question: "Why are functions useful in programming?",
+choices: [
+"They reduce code reuse",
+"They make code reusable and modular",
+"They remove loops",
+"They avoid syntax errors"
+],
+answer: "They make code reusable and modular"
+},
+
+{
+question: "What is a function in Python?",
+choices: [
+"A variable",
+"A group of statements that perform a task",
+"A loop",
+"A data type"
+],
+answer: "A group of statements that perform a task"
+},
+
+{
+question: "Which is a benefit of functions?",
+choices: [
+"Reduce repeated code",
+"Improve readability",
+"Make program modular",
+"All of the above"
+],
+answer: "All of the above"
+},
+
+{
+question: "Which keyword defines a function in Python?",
+choices: [
+"function",
+"define",
+"def",
+"func"
+],
 answer: "def"
 },
 
 {
-question: "What is a function?",
+question: "Which syntax correctly defines a function?",
 choices: [
-"A variable",
-"A group of statements performing a task",
-"A loop",
-"A data type"
+"function add(a,b)",
+"def add(a,b):",
+"define add(a,b)",
+"func add(a,b)"
 ],
-answer: "A group of statements performing a task"
+answer: "def add(a,b):"
 },
 
 {
-question: "Which module is used for plotting in matplotlib?",
-choices: ["pyplot","plot","draw","graph"],
+question: "How do you call a function in Python?",
+choices: [
+"Write the function name",
+"Write the function name with ()",
+"Use call keyword",
+"Use run keyword"
+],
+answer: "Write the function name with ()"
+},
+
+{
+question: "Given function greet(), how do you execute it?",
+choices: [
+"greet",
+"greet()",
+"call greet",
+"run greet"
+],
+answer: "greet()"
+},
+
+{
+question: "A variable created inside a function is called?",
+choices: [
+"Global variable",
+"Local variable",
+"Static variable",
+"External variable"
+],
+answer: "Local variable"
+},
+
+{
+question: "Global variables can be accessed where?",
+choices: [
+"Only inside functions",
+"Only in loops",
+"Anywhere in the program",
+"Only inside classes"
+],
+answer: "Anywhere in the program"
+},
+
+{
+question: "What does this code do?\nfor i in range(5): nums.append(i)",
+choices: [
+"Create list 0-4",
+"Create list 1-5",
+"Create empty list",
+"Error"
+],
+answer: "Create list 0-4"
+},
+
+{
+question: "Which method adds an item to a list?",
+choices: [
+"add()",
+"push()",
+"append()",
+"insertItem()"
+],
+answer: "append()"
+},
+
+{
+question: "What is list comprehension?",
+choices: [
+"A short way to create lists",
+"A method to delete lists",
+"A sorting technique",
+"A loop type"
+],
+answer: "A short way to create lists"
+},
+
+{
+question: "Which is an example of list comprehension?",
+choices: [
+"[x for x in range(5)]",
+"list(range(5))",
+"for x in range(5)",
+"range(5)"
+],
+answer: "[x for x in range(5)]"
+},
+
+{
+question: "What does this create?\n[[0 for j in range(3)] for i in range(3)]",
+choices: [
+"3x3 matrix",
+"1D list",
+"Dictionary",
+"Tuple"
+],
+answer: "3x3 matrix"
+},
+
+{
+question: "What is an advantage of 2D list comprehension?",
+choices: [
+"Short code",
+"Readable",
+"Fast creation of matrix",
+"All of the above"
+],
+answer: "All of the above"
+},
+
+{
+question: "Matplotlib is used for?",
+choices: [
+"Database",
+"Data visualization",
+"Web development",
+"AI models"
+],
+answer: "Data visualization"
+},
+
+{
+question: "Which module is commonly used for plotting?",
+choices: [
+"plotlib",
+"pyplot",
+"draw",
+"visual"
+],
 answer: "pyplot"
 },
 
 {
-question: "Which library is used for numerical computing?",
-choices: ["Pandas","NumPy","Flask","Tkinter"],
-answer: "NumPy"
+question: "What is NumPy?",
+choices: [
+"Networking library",
+"Numerical computing library",
+"Web framework",
+"GUI toolkit"
+],
+answer: "Numerical computing library"
+},
+
+{
+question: "How do you create a NumPy array?",
+choices: [
+"np.create()",
+"np.array()",
+"np.make()",
+"np.list()"
+],
+answer: "np.array()"
 }
 
 ];
