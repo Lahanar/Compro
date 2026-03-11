139d2e3a3055fd9151055987f6de62818421d9f1
Lahanar <144640552+Lahanar@users.noreply.github.com>
2026-03-11 08:23:46 +0700

    Create question.js with Python quiz questions
    Added a set of quiz questions related to Python programming.

---
+++ python-quiz/question.js
@@ -0,0 +1,32 @@
+const questions = [
+
+{
+question: "What keyword is used to define a function in Python?",
+choices: ["function","def","func","define"],
+answer: "def"
+},
+
+{
+question: "What is a function?",
+choices: [
+"A variable",
+"A group of statements performing a task",
+"A loop",
+"A data type"
+],
+answer: "A group of statements performing a task"
+},
+
+{
+question: "Which module is used for plotting in matplotlib?",
+choices: ["pyplot","plot","draw","graph"],
+answer: "pyplot"
+},
+
+{
+question: "Which library is used for numerical computing?",
+choices: ["Pandas","NumPy","Flask","Tkinter"],
+answer: "NumPy"
+}
+
+];
